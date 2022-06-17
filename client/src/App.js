7dfc1c52a88016bbe53cd6c45e3c13fa4ce991a2
chisimdi <chisimdi.ezeanieto@nayalabs.co>
2022-06-17 18:33:34 +0100

    Added some components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,36 @@
+import {
+  ApolloClient, ApolloProvider, createHttpLink, InMemoryCache,
+} from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import MenuBar from './components/MenuBar';
+import Register from './components/Register';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 
 function App() {
   return (
-    <div className="App">App started
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        loremipsom
+      </div>
+    </div>
+  );
+}
+
+export default Home;
